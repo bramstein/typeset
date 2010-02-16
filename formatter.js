1fe74e65b24359b8b2a57f5622e0eb2a776c627b
Bram Stein <b.l.stein@gmail.com>
2010-02-16 17:03:11 +0100

    Reorganized code and rewrote article.

---
+++ formatter.js
@@ -0,0 +1,100 @@
+/*global linebreak*/
+/*requires ../core/object.js*/
+/*requires ../core/array.js*/
+/*requires linebreak.js*/
+
+/*!
+ * Knuth and Plass line breaking algorithm in JavaScript
+ *
+ * Licensed under the new BSD License.
+ * Copyright 2009-2010, Bram Stein
+ * All rights reserved.
+ */
+var formatter = function (measureText, options) {
+	var emWidth = measureText('M'),
+		o = Object.extend({}, formatter.defaults, options);
+
+
+
+	return {
+		center: function (text) {
+			var nodes = [],
+				words = text.split(/\s/),
+				spaceWidth = o.space.width * emWidth,
+				spaceStretch = o.space.stretch * emWidth,
+				spaceShrink = o.space.shrink * emWidth;
+
+			// Although not specified in the Knuth and Plass whitepaper, this box is necessary
+			// to keep the glue from disappearing.
+			nodes.push(linebreak.box(0, ''));
+			nodes.push(linebreak.glue(0, 12, 0));
+
+			words.forEach(function (word, index, array) {
+				nodes.push(linebreak.box(measureText(word), word));
+
+				if (index === array.length - 1) {
+					nodes.push(linebreak.glue(0, 12, 0));
+					nodes.push(linebreak.penalty(0, -linebreak.defaults.infinity, 0));
+				} else {
+					nodes.push(linebreak.glue(0, 12, 0));
+					nodes.push(linebreak.penalty(0, 0, 0));
+					nodes.push(linebreak.glue(spaceWidth, -24, 0));
+					nodes.push(linebreak.box(0, ''));
+					nodes.push(linebreak.penalty(0, linebreak.defaults.infinity, 0));
+					nodes.push(linebreak.glue(0, 12, 0));
+				}
+			});
+			return nodes;
+		},
+		justify: function (text) {
+			var nodes = [],
+				words = text.split(/\s/),
+				spaceWidth = o.space.width * emWidth,
+				spaceStretch = o.space.stretch * emWidth,
+				spaceShrink = o.space.shrink * emWidth;
+
+			words.forEach(function (word, index, array) {
+				nodes.push(linebreak.box(measureText(word), word));
+
+				if (index === array.length - 1) {
+					nodes.push(linebreak.glue(0, linebreak.defaults.infinity, 0));
+					nodes.push(linebreak.penalty(0, -linebreak.defaults.infinity, 1)); 
+				} else {
+					nodes.push(linebreak.glue(spaceWidth, spaceStretch, spaceShrink));
+				}
+			});
+			return nodes;
+		},
+		left: function (text) {
+			var nodes = [],
+				words = text.split(/\s/),
+				spaceWidth = o.space.width * emWidth,
+				spaceStretch = o.space.stretch * emWidth,
+				spaceShrink = o.space.shrink * emWidth;
+
+			words.forEach(function (word, index, array) {
+				nodes.push(linebreak.box(measureText(word), word));
+
+				if (index === array.length - 1) {
+					nodes.push(linebreak.glue(0, linebreak.defaults.infinity, 0));
+					nodes.push(linebreak.penalty(0, -linebreak.defaults.infinity, 1)); 
+				} else {
+					nodes.push(linebreak.glue(0, 12, 0));
+					nodes.push(linebreak.penalty(0, 0, 0));
+					nodes.push(linebreak.glue(spaceWidth, -12, 0)); 
+				}
+			});
+			return nodes;
+		}
+	};
+};
+
+Object.extend(formatter, {
+	defaults: {
+		space: {
+			width: 1 / 3,
+			stretch: 1 / 6,
+			shrink: 1 / 9
+		}
+	}
+});
